170edac4f157d8f32157dd853ff40942ae4a27e5
anurag bhatt <anuragbhatt408@gmail.com>
2022-12-19 10:55:13 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const http = require("node:http");
+
+const PORT = 8000;
+
+// Create a local server to receive data from
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(
+    JSON.stringify({
+      data: "Hello World!",
+    })
+  );
+});
+
+console.log(`server is running is at http://localhost:${PORT}`);
+
+server.listen(PORT);
